72e22178e8dae7898f8a65740f31d7f59018a5e1
hainaweiben <112739514+hainaweiben@users.noreply.github.com>
2023-09-06 23:25:25 +0800

    9/6构建基础的vector.h以及构造内存分配的allocator.h文件

--- vector.h
+++ vector.h
@@ -37,6 +37,78 @@ namespace mystl
 #undef min
 #endif // min
 
+// 模板类: vector 
+// 模板参数 T 代表类型
+template<class T>
+class vector
+{
+    static_assert(!std::is_same<bool, T>::value, "vector<bool> is abandoned in mystl");
+public:
+    // vector 的嵌套型别定义
+    typedef mystl::allocator<T>                      allocator_type;
+    typedef mystl::allocator<T>                      data_allocator;
+
+    typedef typename allocator_type::value_type value_type;
+    typedef typename allocator_type::pointer pointer;
+    typedef typename allocator_type::const_pointer const_pointer;
+    typedef typename allocator_type::reference reference;
+    typedef typename allocator_type::const_reference cost_reference;
+    typedef typename allocator_type::size_type size_type;
+    typedef typename allocator_type::difference_type difference_type;
+
+    typedef value_type* iterator;
+    typedef value_type*                              iterator;
+    typedef const value_type*                        const_iterator;
+    typedef mystl::reverse_iterator<iterator>        reverse_iterator;
+    typedef mystl::reverse_iterator<const_iterator>  const_reverse_iterator;
+    
+    allocator_type get_allocator() { return data_allocator(); }
+    /*
+    **这段代码是在定义一系列类型别名，用于简化对allocator_type成员类型的访问。
+    */
+
+private:
+    iterator begin_; // 表示目前使用空间的头部
+    iterator end_;   // 表示目前使用空间的尾部
+    iterator cap_;   // 表示目前储存空间的尾部
+
+public:
+    //构造、复制、移动、析构函数
+    vector() noexcept
+    { try_init(); }
+
+    explicit vector(size_type n)
+    { fill_init(n,value_type()); }
+
+    vector(size_type n, const value_type& value)
+    { fill_init(n, value); }
+
+
+    private:
+        // helper fuctions
+
+        // initialize / destroy
+        void try_init() noexcept;
+};
+/*************************************************************************/
+//  helper functions
+
+// try_init 函数，若分配失败则忽略，不抛出异常
+template <class T>
+void vector<T>::try_init() noexcept
+{
+    try{
+        begin_ = data_allocator::allocate(16);
+        end_   = begin_;
+        cap_   = begin_ + 16;
+    }
+    catch (...)
+    {
+        begin_ = nullptr;
+        end_   = nullptr;
+        cap_   = nullptr;
+    }
+}
 
 }//namespace mystl end
 #endif // !MYTINYSTL_VECTOR_H_
